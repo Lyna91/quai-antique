8ebe91beea13d686838cabeb70b2ddf9488df2e5
Lyna91 <alinetorteau@gmail.com>
2024-10-26 22:56:47 +0200

    ajout de la page galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,7 @@ import Route from "./Route.js";
 //definir ici vos routes
 export const allRoutes = [
     new Route("/", "Acceuil", "/pages/home.html",),
-    new Route("/Galerie", "Galerie", "/pages/galerie.html"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
 ];
 
 //le titre s'affiche comme ceci : Route.titre - websitename
