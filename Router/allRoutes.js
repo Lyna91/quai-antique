c80d7c49e51fd6b020ebbe86fc23c6a5ebd7499b
Lyna91 <alinetorteau@gmail.com>
2024-10-27 14:04:45 +0100

    ajout dossier resa, fichier allResa

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,6 +8,8 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Modifier mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/allResa", "Les reservations", "/pages/Reservations/allResa.html"),
+
 
 ];
 
